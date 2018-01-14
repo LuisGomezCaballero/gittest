c879b22ced3a9b311da467c7ab6189c9a1b259a7
luisgomezcaballero <luis.gomez.caballero@gmail.com>
2018-01-14 11:39:57 +0100

    Change output message

--- gittest/src/main/java/gittest/Main.java
+++ gittest/src/main/java/gittest/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		System.out.println(
+				"Lorem ipsum dolor sit amet, ius dolor laoreet in, nec gloriatur definiebas ut. Nusquam eloquentiam vix ei, hinc invidunt comprehensam per cu. Iudico tation audire ei nam, impedit graecis est ad, et pro choro noster. Mel an dignissim voluptatum, mei no feugait persequeris.");
 
 	}
 
